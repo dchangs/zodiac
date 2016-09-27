af57ef3d0da76a76198f605de6f21bbe4082f891
debra chang <debraychang@yahoo.com>
2016-09-26 20:58:44 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+var zodiac = [
+	{
+		sign: 'aquarius',
+		elmt: 'Air',
+		image: '<img src="img/aquarius.png" alt="aquarius">',
+		message: 'Aquarius is a fixed Air sign that speaks of inevitability of change. It is a carrier of originality and ideas, one more related to the power of the mind. It speaks of our constant ability to be in touch with the Universe and the field of universal knowledge. People born with the Sun in this sign can be too erratic and unfocused, lost in their constant buzz. They will absorb more than they can metabolize and often turn to any form of humanitarian work just to handle the troubles of humanity in an acceptable manner, for they already see the world the way it should be.'
+	},
+	{
+		sign: 'pisces',
+		elmt: 'Water',
+		image: '<img src="img/pisces.png" alt="pisces">',
+		message: 'The sign of Pisces is a mutable Water sign and represents the deepest oceans in their whole glory. Wide, lost, floating on the surface of the seas, those born with the Sun in Pisces often have trouble discovering their mission and their true path in life. This is a sign of hidden talents, those that aren’t approved by one’s family, and as such it can be the cause of  suffering and feelings of incompetence. Spiritual and sensitive, these people are great missionaries with a cause to follow and a talent to share with the rest of the world.'
+	},
+	{
+		sign: 'aries',
+		elmt: 'Fire',
+		image: '<img src="img/aries.png" alt="aries">',
+		message: 'That temperamental trait of individuals born with the Sun in the elmt of Fire is best seen in a young Aries. They are dynamic and full of energy, often forgetting to slow down, look into the past for some answers and learn from their experiences. Filled with enthusiasm and childish, an Aries is a true leader that will be able to spark your inner fire in no time. When active enough and focused on ways to focus their energy on their true goals, these people are representatives of Fire in its purest and controlled form.'
+	},
+	{
+		sign: 'taurus',
+		elmt: 'Earth',
+		image: '<img src="img/taurus.png" alt="taurus">',
+		message: 'Among the Earth signs, Taurus is the most stable because of its fixed quality. This can make individuals born with the Sun in it quite rigid, stubborn and unmovable, quite irritating to those marked by the elmt of Air. These individuals are wise, nurturing and reliable, with a mission to find physical satisfaction in this lifetime. They build a solid foundation for each relationship they commit to and won’t settle for any sort of material insecurity or fear. They need a strong basis to have some fun and enjoy their deeply calm and emotional nature. Seemingly clingy and tied up, they can be perfect companions in the long run.'
+	},
+	{
+		sign: 'gemini',
+		elmt: 'Air',
+		image: '<img src="img/gemini.png" alt="gemini">',
+		message: 'The sign of Gemini is a mutable sign belonging to the elmt of Air. This makes it the least stable of all signs of the zodiac and people born with the Sun in Gemini can be hard to follow. They are always looking for something new and exciting, never having enough incredible experiences that will satisfy their craving for information and socializing. Their life seems like a search for something or someone to complete them and excite them enough so they can find peace. They need room to explore and show their intellectual and verbal magnificence daily.'
+	},
+	{
+		sign: 'cancer',
+		elmt: 'Water',
+		image: '<img src="img/cancer.png" alt="cancer">',
+		message: 'As a cardinal representative of the elmt of Water, the sign of Cancer is compassionate and caring on first impulse. Without overthinking, those born with the Sun in Cancer will be supportive and caring, with an open mind and heart for all human differences. Once they get hurt, these people close their hearts and become overprotective of those they still love, or their country and their home. Unaware of the lack of danger that lurks around the corner, they can get too attached to their home and even become afraid of leaving it or letting go of their memories.'
+	},
+	{
+		sign: 'leo',
+		elmt: 'Fire',
+		image: '<img src="img/leo.png" alt="leo">',
+		message: 'Just like a middle child among the Fire signs, Leo craves to be the center of attention to everyone around. Belonging to the sign of fixed quality, they represent the personality that is proud and constant, with little room for change. This can make Leo representatives a tad difficult, for they have deep seeded, passionate respect for who they are and are often unaware of the requests of society and other people. Ruled by the Sun, Leo is passion and creativity in its most stable unchangeable form, giving balance to the Universe as we know it.'
+	},
+	{
+		sign: 'virgo',
+		elmt: 'Earth',
+		image: '<img src="img/virgo.png" alt="virgo">',
+		message: 'Virgo is a mutable Earth sign, representing soil that is not that tough and stiff as you always expect from Earth. In general, those born with the Sun in Virgo are less set in their ways than other Earth signs, but still have trouble remaining that flexible attitude when it comes to matters of the heart and their own emotional needs. They took the best from the earthly world though, as reliable and caring, while at the same time changeable enough to be fun to be with. They need to let emotions lead the way from time to time in order to achieve true inner state of satisfaction.'
+	},
+	{
+		sign: 'libra',
+		elmt: 'Air',
+		image: '<img src="img/libra.png" alt="libra">',
+		message: 'The sign of Libra is an Air sign with a Cardinal quality, making people born with the Sun in it potentially innovative and incredibly open for new things. Libras bring balance and harmony into our lives, much faster than their indecisiveness will let you believe. Their main problem is the fall of the Sun in their sign as they need to come to terms with their role of announcers of autumn, the season when all things are dying down. If they accept their personality in all its glory, they become joyful but calm, those few able to mend any relationship or conflict.'
+	},
+	{
+		sign: 'scorpio',
+		elmt: 'Water',
+		image: '<img src="img/scorpio.png" alt="scorpio">',
+		message: 'Scorpio is a fixed Water sign. Slippery and dark, this is a sign of all dark places, dirty rivers that need to be cleansed and ancestors that brought us where we are today. Those born with this Sun sign are intense as they carry memories of their entire family tree on their skin, and have a task to overcome emotional challenges of those that are long gone. In their “underground activities”, digging through their subconscious, these individuals will find pools of greatest energy this planet has, like rivers of lava that burn their way through the stone.'
+	},
+	{
+		sign: 'sagittarius',
+		elmt: 'Fire',
+		image: '<img src="img/sagittarius.png" alt="sagittarius">',
+		message: 'The sign of Sagittarius is the Fire that is out of our reach, mutable in quality and really hard to grasp on. People born with their Sun in this sign are idealistic and always striving for more, aiming higher and higher, reaching for the sky. Representing the end of one season, a Sagittarius is the gentlest of all Fire signs, just like fire that goes out slowly, with all the work done and left behind. Philosophic and idealists, these individuals seem to be the closest to the elmt of Air and often pair with Air individuals for they seem to help them breathe.'
+	},
+	{
+		sign: 'capricorn',
+		elmt: 'Earth',
+		image: '<img src="img/capricorn.png" alt="capricorn">',
+		message: 'The sign of Capricorn is the last Earth sign that opens us up to other worlds, everything that comes “after” and represents our strongest, inevitable faith. Cardinal quality in Capricorn individuals makes them strangely open for change, however closed up they might be for any sort of innovations along the way. Once they find a method that works, they will rarely change it but this doesn’t mean they won’t try many new things, often in secrecy out of fear of failure or making a mistake. Serious workers and career chasers, these individuals know how to reach point B from point A and are valuable as leaders and managers.'
+	}
+]
+
+
+function getInfo() {
+	var name = document.getElementById("name").value
+	var usersign = document.getElementById("sign").value
+
+	for (var i = 0; i < zodiac.length; i++) {
+		if (usersign.toLowerCase() == zodiac[i].sign) {
+			console.log(usersign)
+			document.getElementById('zodiac').textContent = name + ', your zodiac sign is ' + zodiac[i].sign.toUpperCase() + '!'
+			document.getElementById('element').textContent = zodiac[i].elmt
+			document.getElementById('image').innerHTML = zodiac[i].image
+			document.getElementById('forcast').textContent = zodiac[i].message
+			$('#zodiac').toggleClass('animated zoomInDown')
+			return
+		} else {
+			document.getElementById('zodiac').textContent = usersign + " is not a zodiac sign!"
+		}
+	}
+}
+
+$('#image').mouseenter(function() {
+	$('#image').addClass('animated jello')
+	})
+
+$('#image').mouseleave(function() {
+	$('#image').removeClass('animated jello')
+})
